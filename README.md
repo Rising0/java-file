8dd40bb648f045c89c1ef165ce49bdf3eb1c16f1
Rising0 <thiagotavares2005@outlook.com>
2022-10-16 20:54:58 -0300

    User focused version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# javafile-exercise2
+# Java File **(User-Focused Version)**
+
+# Example
+
+<img src="/home/pc/IdeaProjects/Course/img/example.png" />
+
+# Usage
+**Enter the directory where the file will be created.**
+<img src="/home/pc/IdeaProjects/Course/img/usage.png" />
+
+**Enter the number of products you want to register.**
+<img src="/home/pc/IdeaProjects/Course/img/usage2.png" />
+
+**After registering the products, a folder called "out" will be created inside the directory you specified with: Name, Product x Quantity**
+<img src="/home/pc/IdeaProjects/Course/img/usage3.png" />
+<img src="/home/pc/IdeaProjects/Course/img/usage4.png" />
+
+END. You can register as many products as you want, it will be well aligned.
---
+++ src/application/Application.java
@@ -0,0 +1,71 @@
+package application;
+
+import model.entities.Product;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        List<Product> products = new ArrayList<>();
+
+        System.out.print("Enter file path: ");
+        String path = sc.nextLine();
+
+        File filePath = new File(path);
+        boolean success = new File(filePath.getParent() + "/out").mkdir();
+
+        System.out.println("Directory created: " + success);
+
+        System.out.print("How many products do you want to register? ");
+        int productsQuantity = sc.nextInt();
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true))) {
+
+            for (int i = 0; i < productsQuantity; i++) {
+                System.out.printf("\nProduct #%d data:\n", (i + 1));
+
+                sc.nextLine();
+                bufferedWriter.newLine();
+
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Price: $");
+                Double price = sc.nextDouble();
+
+                System.out.print("Quantity: ");
+                Integer quantity = sc.nextInt();
+
+                products.add(new Product(name, price, quantity));
+
+                bufferedWriter.write(products.get(i).getName() + ", " + String.format("%.2f", products.get(i).getPrice()) + ", " + products.get(i).getQuantity());
+            }
+        }
+        catch (RuntimeException exception) {
+            exception.printStackTrace();
+        }
+        catch (IOException exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath.getParent() + "/out/summary.csv", true))) {
+            for (Product product : products) {
+                bufferedWriter.newLine();
+                bufferedWriter.write(product.getName()  + ", " + String.format("%.2f", product.getPrice() * product.getQuantity()));
+            }
+        } catch (IOException exception) {
+            System.out.println("Error: " + exception.getMessage());
+        }
+    }
+}
